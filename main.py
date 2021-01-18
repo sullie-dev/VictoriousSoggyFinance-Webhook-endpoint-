7bbb114c207097b1fbb332c3b1c5d95df2495110
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-01-18 16:22:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from __future__ import absolute_import, division, print_function
+
+
+import os
+import stripe
+
+from flask import Flask, request
+from threading import Thread
+
+
+
+stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
+webhook_secret = os.environ.get("WEBHOOK_SECRET")
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+@app.route("/webhooks", methods=["POST"])
+def webhooks():
+    payload = request.data.decode("utf-8")
+    received_sig = request.headers.get("Stripe-Signature", None)
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, received_sig, webhook_secret
+        )
+    except ValueError:
+        print("Error while decoding event!")
+        return "Bad payload", 400
+    except stripe.error.SignatureVerificationError:
+        print("Invalid signature!")
+        return "Bad signature", 400
+    ch = stripe.issuing.Cardholder.retrieve(
+      event.data.object.cardholder
+    )
+    print(ch.email)
+
+    # print(event)
+
+    return "", 200
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
